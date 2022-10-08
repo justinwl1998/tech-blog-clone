c6b1b5b711bdc055d48c8899f1fa6a511551127e
Justin Liu <justinwl2097@gmail.com>
2022-10-08 16:30:02 -0500

    added blog post handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -23,4 +23,35 @@ router.get('/', async(req, res) => {
     }
 });
 
+router.get('/post/:id', async(req, res) => {
+    try {
+        const postData = await Blogpost.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            ...post,
+        });
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', async (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+})
+
 module.exports = router;
