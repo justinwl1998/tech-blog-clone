322f2447515cc555dce65575a6a9ef75e450d6e7
Justin Liu <justinwl2097@gmail.com>
2022-10-08 15:35:10 -0500

    more work on displaying blog posts on the home page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,20 @@ const { Blogpost, User, Comment } = require('../models');
 
 router.get('/', async(req, res) => {
     try {
-        res.render('homepage');
+        const postData = await Blogpost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        })
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts
+        });
     }
     catch (err) {
         res.status(500).json(err);
